0b723dce8049e93f3d469e815f2a4f56ea5f58c4
phillipe henriques <phillipe_ch@hotmail.com>
2023-03-24 15:36:14 -0300

    Script finalizado

--- EX1 - Batimentos.py
+++ EX1 - Batimentos.py
@@ -4,6 +4,61 @@
 # Adulto sedentario > 70 a 80
 # Idosos 50 a 60
 
-BPM = int(input("Insira o batimento cardiaco"))
-IDADE = int(input("Insira sua idade"))
+bpm = int(input("Insira o batimento cardiaco")) # Coletando batimentos
+idade = int(input("Insira sua idade")) # Coletando idade
 
+# Checando idade menor ou igual a 2 anos
+if idade <= 2:
+    if bpm >= 120:
+        if bpm <=140:
+            print("Batimentos considerados normais")
+        else:
+            print("Batimentos considerados maiores que o normal")
+    else:
+        print("Batimentos considerados menores que o normal")
+
+    # Checando idade de 8 a 17 anos
+
+elif idade >= 8:
+    if idade <= 17:
+
+        # Checando batimentos de 80 a 100
+
+        if bpm >= 80:
+            if bpm <= 100:
+                print("Batimentos considerados normais")
+            else:
+                print("Batimentos considerados maiores que o normal")
+        else:
+            print("Batimentos considerados menores que a média")
+
+    # Idade de 18 a 60 anos
+
+    if idade >= 18:
+        if idade <= 50:
+
+            # Checando batimentos de 70 a 80
+
+            if bpm >= 70:
+                if bpm <= 80:
+                    print("Batimentos considerados normais")
+                else:
+                    print("Batimentos considerados maiores que o normal")
+            else:
+                print("Batimentos considerados menores que o normal")
+
+        # Considerando o paciente idoso já que ele não tem idade menor a 50 anos
+
+        else:
+            bpm >= 50
+            if bpm <= 60:
+                if bpm <= 80:
+                    print("Batimentos considerados normais")
+                else:
+                    print("Batimentos considerados maiores que o normal")
+            else:
+                print("Batimentos considerados menores que o normal")
+
+# Explicando que o script só consegue ler de 0 a 60 anos de idade
+
+else: print("Idade fora do script de análise")
