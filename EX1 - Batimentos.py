0fadc7cbd16ed89dd6981ff21aad2e90b8c1f9a9
phillipe henriques <phillipe_ch@hotmail.com>
2023-03-24 15:03:20 -0300

    EX 1 - Montando exercício

---
+++ EX1 - Batimentos.py
@@ -0,0 +1,9 @@
+# Idade BPM
+# Ate 2 anos > 120 a 140
+# De 8 anos ate 17 anos > 80 a 100
+# Adulto sedentario > 70 a 80
+# Idosos 50 a 60
+
+BPM = int(input("Insira o batimento cardiaco"))
+IDADE = int(input("Insira sua idade"))
+
