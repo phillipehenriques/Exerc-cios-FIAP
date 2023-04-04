ef6bd3b03cb6dabe6bcf04a1840bf5c576532f1b
phillipe henriques <phillipe_ch@hotmail.com>
2023-04-04 16:03:08 -0300

    AT3 sendo pensada

---
+++ AT3 - Sistema de Notas.py
@@ -0,0 +1,28 @@
+
+
+alunos_impares = int(input("Bem vindo a central das notas! \n Por favor, digite a nota do aluno com número impar"))
+alunos_pares = int(input("Agora por favor, digite a nota do aluno com número par"))
+
+list_impares = []
+list_pares = []
+
+#----------------------------
+
+if alunos_impares /1:
+    alunos_impares = int(input("Bem vindo a central das notas! \n Por favor, digite a nota do aluno com número impar"))
+if alunos_impares /2:
+    alunos_pares = int(input("Agora por favor, digite a nota do aluno com número par"))
+
+
+#----------------------------
+
+if alunos_pares < 50:
+    list_pares = int(input("Digite a nota do aluno com número par"))
+else:
+    print("Todas as notas preenchidas")     
+
+if alunos_impares < 49:
+    list_impares = int(input("Digite a nota do aluno com número inpar"))
+else:
+    print("Todas as notas preenchidas") 
+
