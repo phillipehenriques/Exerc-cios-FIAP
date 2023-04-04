345034d8b2af8297935ef2321024fba4ae9b63d6
phillipe henriques <phillipe_ch@hotmail.com>
2023-04-04 12:20:05 -0300

    AT2 Entregue e comentada

--- AT2 - Sistema de Votos Semanais.py
+++ AT2 - Sistema de Votos Semanais.py
@@ -1,12 +1,15 @@
+# sistema de votação
 
 val1 = int(input("Digite o valor de votos da segunda-feira"))
-val2 = int(input())
-val3 = int(input())
-val4 = int(input())
-val5 = int(input())
+val2 = int(input("Digite o valor de votos da terça-feira"))
+val3 = int(input("Digite o valor de votos da quarta-feira"))
+val4 = int(input("Digite o valor de votos da quinta-feira"))
+val5 = int(input("Digite o valor de votos da sexta-feira"))
 
 list = [val1, val2, val3, val4, val5]
 dias_da_semana = ["Segunda-feira", "Terça-Feira", "quarta-feira", "Quinta-Feira", "Sexta-feira"]
+
+# script
 
 maior_valor_da_lista = 0
 for valor in list:
@@ -14,5 +17,7 @@ for valor in list:
         maior_valor_da_lista = valor
         
 posicao_do_maior_valor = list.index(maior_valor_da_lista)
+
+# printando resultado
 
 print(dias_da_semana[posicao_do_maior_valor])
